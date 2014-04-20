be110f4e3b1238c569ecea429961d0b5a15e9757
Chan Nguyen <atbl1511@gmail.com>
2014-04-20 00:19:12 -0700

    Initial Commit

---
+++ MemoryBlock/MMBAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  MMBAppDelegate.h
+//  MemoryBlock
+//
+//  Created by chan on 4/20/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ MemoryBlock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MemoryBlock
+//
+//  Created by chan on 4/20/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMBAppDelegate class]));
+    }
+}
